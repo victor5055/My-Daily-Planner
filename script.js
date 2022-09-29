b0ffd0be261d3f86d3404e1709d15728376399fb
Victor <victor5055@outlook.com>
2022-09-29 16:01:38 -0400

    Adding storage and user value recall.

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ $(document).ready(function () {
 
     var time = moment();
     var hour = moment().hours();
+
+        // storage and user value recall function
+        function plannerActions() {
+            $('.time-block').each(function () {
+                var id = $(this).attr('id');
+                var userInput = localStorage.getItem(id);
+    
+                if (userInput != null) {
+                    $(this).find('.description').val(userInput);
+    
+                }  
+            });
+        }
